b9f46bd32f3c5c0e5679ff5680edb64315d9c91e
oui <hedgehog.oui@gmail.com>
2023-08-30 16:21:06 +0900

    .env周りのロジックを修正

--- astro.config.ts
+++ astro.config.ts
@@ -2,13 +2,10 @@ import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
-import dotenv from 'dotenv'
-
-dotenv.config({ path: `${process.cwd()}/.env.production` })
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.SITE_URL,
+  site: process.env.SITE_URL || 'http://localhost:3000',
   server: {
     host: true,
     port: 3000,
